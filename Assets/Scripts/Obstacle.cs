8963235cc0df52d75bd10c5edb61a81ed925280c
gigi ugulava <gigi.ugulava@hotmail.com>
2020-08-04 16:22:50 +0400

    added primitive color mechanic

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
-
+    public Material CubeMaterial;
     private void OnTriggerEnter(Collider collider) {
         if (!collider.CompareTag("Player"))
         {
-            collider.gameObject.GetComponent<Cube_Movement>().enabled = false;
-            collider.gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-            Debug.Log("haha speed go brr");
+            if (collider.gameObject.GetComponent<Renderer>().material.color == CubeMaterial.color)
+            {
+                collider.gameObject.GetComponent<Renderer>().material.color = gameObject.GetComponent<Renderer>().material.color;
+                Debug.Log("Color Changed");
+            }
+            else if(collider.gameObject.GetComponent<Renderer>().material.color == gameObject.GetComponent<Renderer>().material.color)
+            {
+                collider.gameObject.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            }else
+            {
+                collider.gameObject.GetComponent<Cube_Movement>().enabled = false;
+                collider.gameObject.GetComponent<Renderer>().material.color = gameObject.GetComponent<Renderer>().material.color;
+                Debug.Log("haha speed go brr");
+            }
             return;
         }        
     }
--- Assets/Scripts/PlaceObjects.cs
+++ Assets/Scripts/PlaceObjects.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(GenerateMesh))]
 public class PlaceObjects : MonoBehaviour {
 
-    public TerrainController TerrainController { get; set; }
+    public TerrainController TerrainController { get; set; }    
 
     public void Place() {
         for (int o = 0; o < TerrainController.PlaceableObjects.Length; o++) {
@@ -14,13 +14,13 @@ public class PlaceObjects : MonoBehaviour {
             for (int i = 0; i < numObjects; i++) {
                 int prefabType = o;
                 Vector3 startPoint = RandomPointAboveTerrain();
-
                 RaycastHit hit;
                 if (Physics.Raycast(startPoint, Vector3.down, out hit) && (!TerrainController.Water || hit.point.y > TerrainController.Water.transform.position.y) && hit.collider.CompareTag("Terrain")) {
                     Quaternion orientation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
                     RaycastHit boxHit;
                     if (Physics.BoxCast(startPoint, TerrainController.PlaceableObjects[prefabType].size, Vector3.down, out boxHit, orientation) && boxHit.collider.CompareTag("Terrain")) {
-                        Instantiate(TerrainController.PlaceableObjects[prefabType].prefab﻿, new Vector3(startPoint.x, hit.point.y, startPoint.z), orientation, transform);
+                        var CloneObj = Instantiate(TerrainController.PlaceableObjects[prefabType].prefab﻿, new Vector3(startPoint.x + i, hit.point.y, startPoint.z), orientation, transform);
+                        CloneObj.GetComponent<Renderer>().material.color = RandomColor();
                     }
                     //Debug code. To use, uncomment the giant thingy below
                     //Debug.DrawRay(startPoint, Vector3.down * 10000, Color.blue);
@@ -38,6 +38,10 @@ public class PlaceObjects : MonoBehaviour {
             transform.position.y + TerrainController.TerrainSize.y * 2,
             Random.Range(transform.position.z - TerrainController.TerrainSize.z / 2, transform.position.z + TerrainController.TerrainSize.z / 2)
         );
+    }
+    private Color RandomColor()
+    {
+        return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 
     //code to help visualize the boxcast
